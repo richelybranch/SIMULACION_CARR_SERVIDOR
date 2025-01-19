c7589bb9c18e971f8d9923a70b2a4fc7c719f8d6
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2025-01-19 15:38:05 -0500

    para aws 2

--- index.js
+++ index.js
@@ -150,6 +150,6 @@ app.use((req, res) => {
     res.status(404).json({ mensaje: 'Ruta no encontrada' });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor ejecutándose en http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
